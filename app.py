b0f115b915016cd488210acf331ea25f2f7e493d
sabagogila2 <sabaogogilashvili@gmail.com>
2025-01-06 10:52:23 +0400

    commit 2

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 
 
+from flask import Flask
+
+# Initialize the Flask app
+app = Flask(__name__)
+
+# Import your routes
+from routes import *
+
 if __name__ == '__main__':
-   from routes import *
-   app.run(debug=True)
+    app.run(debug=True)
 
 
